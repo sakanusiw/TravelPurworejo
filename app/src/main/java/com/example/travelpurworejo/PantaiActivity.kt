ffce2feb15de99844af8ac76712ea65b10f82437
sakanusiw <saka.interisti@gmail.com>
2023-08-09 21:20:52 +0700

    added activity_pantai

---
+++ app/src/main/java/com/example/travelpurworejo/PantaiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.travelpurworejo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PantaiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantai)
+    }
+}
