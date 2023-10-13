fc5361346d5406ae21fb60dc778e9638020eeb60
sakanusiw <saka.interisti@gmail.com>
2023-10-13 14:11:47 +0700

    edited Pantai Activity and added images

---
+++ app/src/main/java/com/example/travelpurworejo/RecyclerView/PantaiAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.travelpurworejo.RecyclerView
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
+import com.example.travelpurworejo.R
+
+class PantaiAdapter(private val dataList: List<Int>) : RecyclerView.Adapter<PantaiAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_pantai_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val imageResource = dataList[position]
+        holder.imageButton.setImageResource(imageResource)
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageButton: ImageButton = itemView.findViewById(R.id.imageButton3)
+    }
+}
